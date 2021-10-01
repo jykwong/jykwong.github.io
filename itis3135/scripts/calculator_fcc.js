25f19f621aeb733dbd9c2cc755830a23c32a6d65
Kwong-Yuankit <agp.justin@gmail.com>
2021-10-01 16:09:43 -0400

    calculator

--- itis3135/scripts/calculator_fcc.js
+++ itis3135/scripts/calculator_fcc.js
@@ -1,13 +1,23 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator_keys');
+const display = document.querySelector('.calculator_display');
 
 //listen for all keypresses and determine the type of key that is pressed
 keys.addEventListener('click', e => {
     if (e.target.matches('button')){
         const key = e.target;
         const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        const previousKeyType = calculator.dataset.previousKeyType
+        Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'));
         if (!action) {
             console.log('number key!');
+            if (displayedNum === '0' || previousKeyType === 'operator') {
+                display.textContent = keyContent;
+            } else {
+                display.textContent = displayedNum + keyContent;
+            }
         }
         if (
             action === 'add' ||
@@ -16,68 +26,40 @@ keys.addEventListener('click', e => {
             action === 'divide'
         ){
             console.log('operator key!');
+            key.classList.add('is-depressed');
+            calculator.dataset.previousKeyType = 'operator';
+
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
         }
         if (action === 'decimal') {
             console.log('decimal key');
+            display.textContent = displayedNum + '.';
         }
         if (action === 'clear') {
             console.log('clear key!');
         }
         if (action === 'calculate') {
             console.log('equal key');
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+
+            display.textContent = calculate(firstValue, operator, secondValue);
         }
     }
 })
+const calculate = (n1, operator, n2) => {
+    let result = '';
 
-//get values of the number of that was clicked and display current number
-const display = document.querySelector('.calculator_display');
-
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNum = display.textContent;
-        if (!action) {
-            if (displayedNum === '0') {
-                display.textContent = keyContent;
-            } else {
-                display.textContent = displayedNum + keyContent;
-            }
-        }
-        if (action === 'decimal') {
-            display.textContent = displayedNum + '.';
-        }
-        if (
-            action === 'add' ||
-            action === 'subtract' ||
-            action === 'multiply' ||
-            action === 'divide'
-        ) {
-            key.classList.add('is-depressed');
-            Array.from(key.parentNode.children)
-            .forEach(k => k.classList.remove('is-depressed'));
-        }
+    if (operator ==='add') {
+        result = parseFloat(n1) + parseFloat(n2);
+    } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2);
+    } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2);
+    } else if (operator ==='divide') {
+        result = parseFloat(n1) / parseFloat(n2);
     }
-})
-
-
-
-
-
-/*
-        // remove .is-depressed class from all keys
-        Array.from(key.parentNode.children)
-        .forEach(k => k.classList.remove('is-depressed'));
-        calculator.dataset.previousKeyType = 'operator';
-
-        const previousKeyType = calculator.dataset.previousKeyType;
-
-        if (!action) {
-            if (displayedNum === '0' || previousKeyType === 'operator') {
-                display.textContent = keyContent;
-            } else {
-                display.textContent = displayedNum + keyContent;
-            }
-        }
-*/
+    return result;
+}
