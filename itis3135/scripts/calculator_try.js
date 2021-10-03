7dedba6e33cfd3edbbc0065d8b45673df85de974
Kwong-Yuankit <agp.justin@gmail.com>
2021-10-03 16:27:09 -0400

    calc again

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,2 +1,23 @@
-var firstNumber = document.getElementById('number');
+var firstNumber = document.querySelectorAll('number');
+var operator = document.querySelectorAll('operator');
+var secondNumber = document.querySelectorAll('number');
 
+
+
+function calculate(firstNumber, operator, secondNumber) {
+    var 
+    var result = '';
+    if (operator == "+") {
+        result = parseFloat(firstNumber) + parseFloat(secondNumber);
+    }
+    if (operator == "-") {
+        result = parseFloat(firstNumber) - parseFloat(secondNumber);
+    }
+    if (operator == "*") {
+        result = parseFloat(firstNumber) * parseFloat(secondNumber);
+    }
+    if (operator == "÷") {
+        result = parseFloat(firstNumber) / parseFloat(secondNumber);
+    }
+    return result;
+}
