9b6ce8030c3a8c5a0f79634a6829a8caf5132b56
Kwong-Yuankit <agp.justin@gmail.com>
2021-11-04 21:16:45 -0400

    Update brand.js

--- itis3135/brand/scripts/brand.js
+++ itis3135/brand/scripts/brand.js
@@ -1,4 +1,23 @@
 function submit_data() {
-    let name = document.getElementsByName("fname").value;
+    let name = document.getElementById("fname").value;
     console.log(name);
+    let email = document.getElementById("email").value;
+    console.log(email);
+    let phone = document.getElementById("phone").value;
+    console.log(phone);
+    let newsLetterEmail = document.getElementById("email_newsletter").value;
+    console.log(newsLetterEmail);
+    let newsLetterWebsite = document.getElementById("website").value;
+    console.log(newsLetterWebsite);
+    let newsLetterTextMsg = document.getElementById("text").value;
+    console.log(newsLetterTextMsg);
+    let newsLetterSocials = document.getElementById("social").value;
+    console.log(newsLetterSocials);
+    let interestedYes = document.getElementById("yes").value;
+    console.log(interestedYes);
+    let interestedNo = document.getElementById("no").value;
+    console.log(interestedNo);
+    let comments = document.getElementById("suggestions").value;
+    console.log(comments);
+
 }
