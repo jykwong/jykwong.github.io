4a80d21cd4a30c5603d12e85afe50903dec32bac
Kwong-Yuankit <agp.justin@gmail.com>
2021-11-04 21:31:08 -0400

    123

--- itis3135/brand/scripts/brand.js
+++ itis3135/brand/scripts/brand.js
@@ -5,19 +5,12 @@ function submit_data() {
     console.log(email);
     let phone = document.getElementById("phone").value;
     console.log(phone);
-    let newsLetterEmail = document.getElementById("email_newsletter").value;
-    console.log(newsLetterEmail);
-    let newsLetterWebsite = document.getElementById("website").value;
-    console.log(newsLetterWebsite);
-    let newsLetterTextMsg = document.getElementById("text").value;
-    console.log(newsLetterTextMsg);
-    let newsLetterSocials = document.getElementById("social").value;
-    console.log(newsLetterSocials);
-    let interestedYes = document.getElementById("yes").value;
-    console.log(interestedYes);
-    let interestedNo = document.getElementById("no").value;
-    console.log(interestedNo);
     let comments = document.getElementById("suggestions").value;
     console.log(comments);
 
+    var display = document.getElementById("data");
+    display.innerHTML = ("<p>" + name + "</p>");
+    display.innerHTML += ("<p>" + email + "</p>");
+    display.innerHTML += ("<p>" + phone + "</p>");
+    display.innerHTML += ("<p>" + comments + "</p>");
 }
