ddcdfe45f8bb5783ba4376864cd11d13ba4d9af7
Kwong-Yuankit <agp.justin@gmail.com>
2021-11-18 23:23:01 -0500

    ../

--- itis3135/scripts/Kwong-Activity14.js
+++ itis3135/scripts/Kwong-Activity14.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	
-}); // end ready
+    $.getJSON("team.json", function(data) {
+        $.each(data, function() {
+            $.each(this, function(key, value) {
+                $("#team").append(
+                    "Month: " + value.month + "<br>" +
+                    "Title: " + value.title + "<br>" +
+                    "Speaker: " + value.speaker + "<br>" +
+                    "Image: " + value.image + "<br>" +
+                    "Text: " + value.text + "<br><br>"
+                ); //end append
+            }); //end each inner
+        }); //end each outer
+    }); //end getJSON
+}); //end ready
