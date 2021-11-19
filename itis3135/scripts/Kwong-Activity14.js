5851760db86703c2c1ea3e3f1195f1f6b613279e
Kwong-Yuankit <agp.justin@gmail.com>
2021-11-19 02:06:25 -0500

    Update Kwong-Activity14.js

--- itis3135/scripts/Kwong-Activity14.js
+++ itis3135/scripts/Kwong-Activity14.js
@@ -23,9 +23,9 @@ $(document).ready(function() {
                     $.each(data, function() {
                         $.each(this, function(key, value) {
                             $("main").append(
-                                "<h1>" + value.title + "<br>" +
-                                "<h2>" + value.month + "<br>" +
-                                "<h3>" + value.speaker + "<br>" +
+                                "<h1>" + value.title + "</h1><br>" +
+                                "<h2>" + value.month + "</h2><br>" +
+                                "<h3>" + value.speaker + "</h3><br>" +
                                 "<img src=" + value.image + ">" + "<br>" +
                                 "<p>" + value.text + "</p><br><br>"
                             )
