eda60784c74e1c4c661d27f35c6d062a4eaade59
Kwong-Yuankit <agp.justin@gmail.com>
2021-11-19 00:49:42 -0500

    Update Kwong-Activity14.js

--- itis3135/scripts/Kwong-Activity14.js
+++ itis3135/scripts/Kwong-Activity14.js
@@ -1,21 +1,44 @@
+
 $(document).ready(function() {
     $("#nav_list li").click(function() {
         let title = $(this).children('a').attr("title");
         var filePath = "json_files/" + title + ".json";
         console.log(filePath);
 
-        $.getJSON(filePath, function(data) {
-            $.each(data, function() {
-                $.each(this, function(key, value) {
-                    $("#team").append(
-                        "Month: " + value.month + "<br>" +
-                        "Title: " + value.title + "<br>" +
-                        "Speaker: " + value.speaker + "<br>" +
-                        "Image: " + value.image + "<br>" +
-                        "Text: " + value.text + "<br><br>"
-                    ); //end append
-                }); //end each inner
-            }); //end each outer
-        }); //end getJSON
-    }); //end ready
-});
+        $.ajax({
+            type: "get",
+            url: "team.json",
+            beforeSend: function() {
+                $("#team").html("Loading...");
+            },
+            timeout: 10000,
+            error: function(xhr, status, error) {
+                alert("Error: " + xhr.status + " - " + error);
+            },
+            dataType: "json",
+            success: function(data) {
+                console.log(data); //print data' to conso1e
+                $("#team").html("");
+                $.getJSON("team.json", function(data) {
+                    $.each(data, function() {
+                        $.each(this, function(key, value) {
+                            $("main h1").html(
+                                value.title
+                            );
+                            $("main h2").html(
+                                value.month
+                            )
+                            $("main h3").html(
+                                value.speaker
+                            )
+                            $("main img").attr("src", value.image)
+                            $("main p").html(
+                                value.text
+                            )
+                        });
+                    });
+                });
+            }//end success function
+        }); //end ajax method
+    }); //end nav_list click
+}); //end ready
