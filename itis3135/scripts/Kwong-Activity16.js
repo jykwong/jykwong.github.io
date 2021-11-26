ee65231127aaa9fe4e668c99d602431d21e76021
Kwong-Yuankit <agp.justin@gmail.com>
2021-11-26 15:39:42 -0500

    Update Kwong-Activity16.js

--- itis3135/scripts/Kwong-Activity16.js
+++ itis3135/scripts/Kwong-Activity16.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
-
-});
+    var url = "https://www.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+    
+    $.getJSON(url, function(data) {
+        var html = "";
+        $.each(data.items, function(i, item) {
+            html += "<h2>" + item.title + "</h2>";
+            html += "<img src=" + item.media.m  + ">";
+            html += "<p></p>";
+        })//end .each
+        $("#new_building").html(html);
+    })//end getJSON
+});//end doc ready
