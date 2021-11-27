ff456a4434d23dbf4b837c63e694e323a2901867
Kwong-Yuankit <agp.justin@gmail.com>
2021-11-26 23:45:23 -0500

    Update Kwong-Activity16.js

--- itis3135/scripts/Kwong-Activity16.js
+++ itis3135/scripts/Kwong-Activity16.js
@@ -6,9 +6,8 @@ $(document).ready(function(){
         var count = 1;
         $.each(data.items, function(i, item) {
            // html += "<h2>" + item.title + "</h2>";
-            html += "<a href=" + item.media.m + ">" + "<img src=" + item.media.m  + "data-lightbox=flickr" + count + "\"" + "data-title=" + item.title + "></a>";
+            html += "<a href=" + item.media.m + ">" + "<img src=" + item.media.m  + "data-lightbox=\"flickr" + count + "\"" + "data-title=" + item.title + "></a>";
             //html += "<img src=" + item.media.m  + ">"; from book
-            html += "<p></p>";
             count++;
         })//end .each
         $("#new_building").html(html);
