2c85c4f7cdd463f9ca3938002543c4f9c7ae3f0d
Kwong-Yuankit <agp.justin@gmail.com>
2021-11-27 00:10:20 -0500

    Update Kwong-Activity16.js

--- itis3135/scripts/Kwong-Activity16.js
+++ itis3135/scripts/Kwong-Activity16.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    var url = "https://api.flickr.com/services/feeds/photos_public.gne?id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
+    var url = "https://api.flickr.com/services/feeds/photos_public.gne?" +
+            "id=82407828@N07&format=json&jsoncallback=?&tags=vectacorpbuilding";
     
     $.getJSON(url, function(data) {
         var html = "";
         $.each(data.items, function(i, item) {
-           // html += "<h2>" + item.title + "</h2>";
-            html += "<a href=" + item.media.m + ">" + "<img src=" + item.media.m  + "data-lightbox=\"" + "flickr\"" + "data-title=" + item.title + "></a>";
+            html += "<a href=" + item.media.m + " data-lightbox=\"" + "flickr\"" + " data-title=" + item.title + ">" + "<img src=" + item.media.m + "></a>";
             //html += "<img src=" + item.media.m  + ">"; from book
         })//end .each
         $("#new_building").html(html);
