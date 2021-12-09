fba3f707e3b72ca1c208dbe18aa439205cf0d9be
Kwong-Yuankit <agp.justin@gmail.com>
2021-12-09 02:51:20 -0500

    p4

---
+++ itis3135/project/scripts/index_script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $.ajax({
+        type: "get",
+        url: "json_files/index_img.json",
+        beforeSend: function() {
+            $("#about, #menu, #gallery, #order").html("Loading...");},
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);},
+        dataType: "json",
+        success: function(data) {
+            console.log(data); //print data' to conso1e
+            $("#about, #menu, #gallery, #order").html("");
+            $.getJSON("json_files/index_img.json", function(data) {
+                $(#about).append(
+                    "<a href=\"about.html\"><img src=" + data.images[0] +" alt=\"about\" ></a>"
+                );
+                $(#menu).append(
+                    "<a href=\"menu.html\"><img src=" + data.images[1] +" alt=\"menu\" ></a>"
+                );
+                $(#gallery).append(
+                    "<a href=\"gallery.html\"><img src=" + data.images[2] +" alt=\"gallery\" ></a>"
+                );
+                $(#order).append(
+                    "<a href=\"order.html\"><img src=" + data.images[3] +" alt=\"order\" ></a>"
+                );
+            });// end getJSON
+        }//end success function
+    }); //end ajax method for appetizer
+})
+
+/*
+$.getJSON("json_files/index_img.json", function(data) {
+                $.each(data, function() {
+                    $.each(this, function(key, value) {
+                        $("#about").append(
+                            "
+                        );//end each appetizer
+                    });//end each data function(key,value)
+                }); //end each(data,function())
+            });// end getJSON
+*/
