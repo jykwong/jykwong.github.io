4147946dc9c0b91df97ecd4f475159f19fcb15f3
Kwong-Yuankit <agp.justin@gmail.com>
2021-12-09 03:52:09 -0500

    Update index_script.js

--- itis3135/project/scripts/index_script.js
+++ itis3135/project/scripts/index_script.js
@@ -9,15 +9,23 @@ $(document).ready(function(){
             alert("Error: " + xhr.status + " - " + error);},
         dataType: "json",
         success: function(data) {
-            console.log(data); //print data' to conso1e
+            console.log(data.images[0]); //print data' to conso1e
             $("#about, #menu, #gallery, #order").html("");
             $.getJSON("json_files/index_img.json", function(data) {
                 $.each(data, function() {
                     $.each(this, function(key, value) {
                         $("#about").append(
-                            "<img src='" + value.image[key] + "'>"
-                        );//end each appetizer
-                        
+                            "<img src='" + value.images[0].image + "'>"
+                        );//end each about
+                        $("#menu").append(
+                            "<img src='" + value.images[1].image + "'>"
+                        );//end each menu
+                        $("#gallery").append(
+                            "<img src='" + value.images[2].image + "'>"
+                        );//end each gallery
+                        $("#order").append(
+                            "<img src='" + value.images[3].image + "'>"
+                        );//end each order
                     });//end each data function(key,value)
                 }); //end each(data,function())
             });// end getJSON
